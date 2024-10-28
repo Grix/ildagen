ba636c8f55e45e0a02e53ba3768476e9c08b6ade
Gitle Mikkelsen <gitlem@gmail.com>
2024-10-28 20:18:23 +0100

    Update linux version of dacwrapper

--- src/dacwrapper/dacwrapper_linux/build.sh
+++ src/dacwrapper/dacwrapper_linux/build.sh
@@ -9,11 +9,10 @@ g++ -Wall -std=c++14 -fPIC -O2 -c ../dacwrapper/HeliosDac.cpp &&
 g++ -Wall -std=c++14 -fPIC -O2 -c ../dacwrapper/Device_Helios.cpp &&
 g++ -Wall -std=c++14 -fPIC -O2 -c ../dacwrapper/plt-posix.c &&
 g++ -Wall -std=c++14 -fPIC -O2 -c ../dacwrapper/idn.cpp &&
-g++ -Wall -std=c++14 -fPIC -O2 -c ../dacwrapper/Device_IDN.cpp &&
 g++ -Wall -std=c++14 -fPIC -O2 -c ../dacwrapper/idnServerList.cpp &&
 gcc -Wall -fPIC -O2 -c ../dacwrapper/etherdream.c &&
 g++ -Wall -std=c++14 -fPIC -O2 -c ../dacwrapper/Device_Etherdream_unix.cpp &&
 g++ -Wall -std=c++14 -fPIC -O2 -c ../dacwrapper/dacwrapper.cpp &&
-g++ -shared -o libdacwrapper.so ./dacwrapper.o ./LaserdockDevice.o ./LaserdockDevice_desktop.o ./LaserdockDeviceManager.o ./LaserdockDeviceManager_desktop.o ./Device_LaserDock.o ./LaserCubeNetwork.o ./Device_LaserDockNetwork.o ./HeliosDac.o ./Device_Helios.o ./plt-posix.o ./idn.o ./Device_IDN.o ./idnServerList.o ./etherdream.o ./Device_Etherdream_unix.o ./libusb-1.0.so &&
+g++ -shared -o libdacwrapper.so ./dacwrapper.o ./LaserdockDevice.o ./LaserdockDevice_desktop.o ./LaserdockDeviceManager.o ./LaserdockDeviceManager_desktop.o ./Device_LaserDock.o ./LaserCubeNetwork.o ./Device_LaserDockNetwork.o ./HeliosDac.o ./Device_Helios.o ./plt-posix.o ./idn.o ./idnServerList.o ./etherdream.o ./Device_Etherdream_unix.o ./libusb-1.0.so &&
 patchelf --set-rpath '$ORIGIN' ./libdacwrapper.so
 
